9a251024b3ce65390440a0ebd13877a8fdeacc3b
gary0130 <gary880130@gmail.com>
2016-03-29 23:20:33 +0800

    0329

--- src/java07/Main.java
+++ src/java07/Main.java
@@ -1,35 +1,32 @@
 package java07;
 
-
 import java.util.Scanner;
 
 public class Main {
 
-	static Scanner scn=new Scanner(System.in);
-	
+	static Scanner scn = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
 		System.out.println("質數");
 		System.out.println("輸入最大的值");
-		int po=scn.nextInt();
-		
-		for(int i=3 ;i<po;i++)
-		{
-			
-			for(int j=2;j<i;j++)
-			{
-				
-				if (i % j==0) {
+		int po = scn.nextInt();
+
+		for (int i = 3; i < po; i++) {
+			int p = 1;
+			for (int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					p = 0;
 					break;
 				}
-				if(j==(i-1)){
-					System.out.println(i);
-				}
-				
-			}
 			
+			}
+			if (p == 1) {
+				System.out.println(i);
+			}
 		}
-		
+
 	}
 
 }
